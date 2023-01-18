68ed31106135e83cbd2925cb41bdd5b4cb1db321
MARCELO DO AMARAL SALA <dam.marcelo.dosala@gmail.com>
2023-01-18 13:29:56 +0100

    creacion de carpetas y ficheros ya hechos en el anterior proyecto erroneo

---
+++ lib/game/VideoGame.dart
@@ -0,0 +1,29 @@
+import 'package:flame/game.dart';
+import 'package:flame_tiled/flame_tiled.dart';
+
+import '../players/MainPlayer.dart';
+
+class VideoGame extends FlameGame {
+  VideoGame();
+
+  @override
+  Future<void>? onLoad() async {
+    await super.onLoad();
+    await images.loadAll([
+      'block.png',
+      'ember.png',
+      'ground.png',
+      'heart_half.png',
+      'heart.png',
+      'star.png',
+      'water_enemy.png',
+    ]);
+
+    TiledComponent mapComponent =
+    await TiledComponent.load('grass.tmx', Vector2(32,32));
+    add(mapComponent);
+
+    MainPlayer mainPlayer = MainPlayer(position: Vector2(200, 200));
+    add(mainPlayer);
+  }
+}
