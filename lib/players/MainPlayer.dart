8c00f43207533a6bcfb47eac043b7e24e32d306d
MARCELO DO AMARAL SALA <dam.marcelo.dosala@gmail.com>
2023-01-19 19:57:28 +0100

    funcionalidad del teclado

--- lib/players/MainPlayer.dart
+++ lib/players/MainPlayer.dart
@@ -10,6 +10,9 @@ class MainPlayer extends SpriteAnimationComponent
     required super.position,
   }) : super(size: Vector2.all(32), anchor: Anchor.center);
 
+  int horizontalMove = 0;
+  int verticalMove = 0;
+
   @override
   Future<void> onLoad() async {
     animation = SpriteAnimation.fromFrameData(
@@ -24,6 +27,32 @@ class MainPlayer extends SpriteAnimationComponent
 
   @override
   bool onKeyEvent(RawKeyEvent event, Set<LogicalKeyboardKey> keysPressed) {
+    horizontalMove = 0;
+    verticalMove = 0;
+    horizontalMove += (keysPressed.contains(LogicalKeyboardKey.keyA) ||
+            keysPressed.contains(LogicalKeyboardKey.arrowLeft))
+        ? -1
+        : 0;
+    horizontalMove += (keysPressed.contains(LogicalKeyboardKey.keyD) ||
+            keysPressed.contains(LogicalKeyboardKey.arrowRight))
+        ? 1
+        : 0;
+
+    verticalMove += (keysPressed.contains(LogicalKeyboardKey.keyW) ||
+            keysPressed.contains(LogicalKeyboardKey.arrowUp))
+        ? -1
+        : 0;
+    verticalMove += (keysPressed.contains(LogicalKeyboardKey.keyS) ||
+            keysPressed.contains(LogicalKeyboardKey.arrowDown))
+        ? 1
+        : 0;
+
     return true;
+  }
+
+  @override
+  void update(double dt) {
+    position.add(Vector2((10.0 * horizontalMove), (10.0 * verticalMove)));
+    super.update(dt);
   }
 }
